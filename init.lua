afb71918bc67a12017f6ab5ec9d3e2638c0e1eca
eng-alecrim <lucasalecrimvieira@gmail.com>
2024-02-06 22:30:51 -0300

    mudei de ThePrimeagen > Alecrim

--- init.lua
+++ init.lua
@@ -1,7 +1,7 @@
-require("theprimeagen.set")
-require("theprimeagen.remap")
+require("alecrim.set")
+require("alecrim.remap")
 
-require("theprimeagen.lazy_init")
+require("alecrim.lazy_init")
 
 -- DO.not
 -- DO NOT INCLUDE THIS
@@ -15,7 +15,7 @@ require("theprimeagen.lazy_init")
 -- DO.not
 
 local augroup = vim.api.nvim_create_augroup
-local ThePrimeagenGroup = augroup('ThePrimeagen', {})
+local Alecrim = augroup('Alecrim', {})
 
 local autocmd = vim.api.nvim_create_autocmd
 local yank_group = augroup('HighlightYank', {})
@@ -42,13 +42,13 @@ autocmd('TextYankPost', {
 })
 
 autocmd({"BufWritePre"}, {
-    group = ThePrimeagenGroup,
+    group = Alecrim,
     pattern = "*",
     command = [[%s/\s\+$//e]],
 })
 
 autocmd('LspAttach', {
-    group = ThePrimeagenGroup,
+    group = Alecrim,
     callback = function(e)
         local opts = { buffer = e.buf }
         vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
